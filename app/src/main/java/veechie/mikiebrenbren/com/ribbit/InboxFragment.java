3a9cb7fcaa3a77690c9e29c45514eb60815a69bf
Michael Brennan <mikiebrenbren@gmail.com>
2015-02-21 20:05:42 -0700

    configured the tab layouts, added two fragment classes that are generated when the tabs are activated

---
+++ app/src/main/java/veechie/mikiebrenbren/com/ribbit/InboxFragment.java
@@ -0,0 +1,23 @@
+package veechie.mikiebrenbren.com.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by michaelbrennan on 2/21/15.
+ */
+public class InboxFragment extends ListFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+        return rootView;
+
+    }
+}
